bd77662b38a849094174bcfd9e20c8c4c5e4480d
Cameron <cpolly@pgh.yearup.org>
2024-12-05 14:15:43 -0500

    Completed Inventory DAO

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -3,20 +3,41 @@ package com.yearup.dealership.db;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class InventoryDao {
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public InventoryDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     public void addVehicleToInventory(String vin, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO inventory (dealership_id, vin) VALUES (?,?)")) {
+            preparedStatement.setInt(1, dealershipId);
+            preparedStatement.setString(2, vin);
+
+
+            int rows = preparedStatement.executeUpdate();
+            System.out.println("Rows Inserted: " + rows);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
-        // TODO: Implement the logic to remove a vehicle from the inventory
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "DELETE FROM inventory WHERE vin = ?")) {
+            preparedStatement.setString(1, vin);
+
+            int rows = preparedStatement.executeUpdate();
+            System.out.println("Rows Deleted: " + rows);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
