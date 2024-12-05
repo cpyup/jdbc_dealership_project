0917fdb304618ae1b44eea9f255a642fea5ebcf4
Cameron <cpolly@pgh.yearup.org>
2024-12-05 14:56:00 -0500

    Implemented Adding New Sales Contracts

--- src/main/java/com/yearup/dealership/db/SalesDao.java
+++ src/main/java/com/yearup/dealership/db/SalesDao.java
@@ -4,17 +4,37 @@ import com.yearup.dealership.models.SalesContract;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class SalesDao {
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public SalesDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     public void addSalesContract(SalesContract salesContract) {
-        // TODO: Implement the logic to add a sales contract
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO sales_contracts (id, contract_date, name, email, vin, sales_tax, recording_fee, processing_fee, total_cost, financed, monthly_payment) VALUES (?,?,?,?,?,?,?,?,?,?,?)")) {
+            preparedStatement.setInt(1, salesContract.getContractId());
+            preparedStatement.setDate(2, Date.valueOf(salesContract.getSaleDate()));
+            preparedStatement.setString(3, salesContract.getName());
+            preparedStatement.setString(4, salesContract.getEmail());
+            preparedStatement.setString(5, salesContract.getVin());
+            preparedStatement.setDouble(6, salesContract.getSalesTax());
+            preparedStatement.setDouble(7, salesContract.getRecordingFee());
+            preparedStatement.setDouble(8,salesContract.getProcessingFee());
+            preparedStatement.setDouble(9,salesContract.getTotalCost());
+            preparedStatement.setBoolean(10, salesContract.isFinanced());
+            preparedStatement.setDouble(11, salesContract.getMonthlyPayment());
+
+            int rows = preparedStatement.executeUpdate();
+            System.out.println("Rows Inserted: " + rows);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/com/yearup/dealership/models/SalesContract.java
+++ src/main/java/com/yearup/dealership/models/SalesContract.java
@@ -6,19 +6,40 @@ public class SalesContract {
     private int contractId;
     private String vin;
     private LocalDate saleDate;
-    private double price;
+    private double totalCost;
+    private String name;
+    private String email;
+    private double salesTax;
+    private double recordingFee;
+    private double processingFee;
+    private boolean financed;
+    private double monthlyPayment;
 
-    public SalesContract(String vin, LocalDate saleDate, double price) {
+    public SalesContract(String vin, LocalDate saleDate, double totalCost) {
         this.vin = vin;
         this.saleDate = saleDate;
-        this.price = price;
+        this.totalCost = totalCost;
+        this.name = "TESTING_NAME";
+        this.email = "TESTING_EMAIL";
+        this.salesTax = 0;
+        this.recordingFee = 0;
+        this.processingFee = 0;
+        this.financed = false;
+        this.monthlyPayment = 0;
     }
 
-    public SalesContract(int contractId, String vin, LocalDate saleDate, double price) {
+    public SalesContract(int contractId, String vin, LocalDate saleDate, double totalCost, String name, String email, double salesTax, double recordingFee, double processingFee, boolean financed, Double monthlyPayment) {
         this.contractId = contractId;
         this.vin = vin;
         this.saleDate = saleDate;
-        this.price = price;
+        this.totalCost = totalCost;
+        this.name = name;
+        this.email = email;
+        this.salesTax = salesTax;
+        this.recordingFee = recordingFee;
+        this.processingFee = processingFee;
+        this.financed = financed;
+        this.monthlyPayment = monthlyPayment;
     }
 
     public int getContractId() {
@@ -45,12 +66,60 @@ public class SalesContract {
         this.saleDate = saleDate;
     }
 
-    public double getPrice() {
-        return price;
+    public double getTotalCost() {
+        return totalCost;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(double recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinanced() {
+        return financed;
+    }
+
+    public void setFinanced(boolean financed) {
+        this.financed = financed;
+    }
+
+    public double getMonthlyPayment() {
+        return monthlyPayment;
+    }
+
+    public void setMonthlyPayment(double monthlyPayment) {
+        this.monthlyPayment = monthlyPayment;
+    }
+
+    public double getSalesTax(){
+        return salesTax;
     }
 
     @Override
@@ -59,7 +128,7 @@ public class SalesContract {
                 "contractId=" + contractId +
                 ", vin='" + vin + '\'' +
                 ", saleDate=" + saleDate +
-                ", price=" + price +
+                ", totalCost" + totalCost +
                 '}';
     }
 }
